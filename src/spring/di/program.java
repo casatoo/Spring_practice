3d06abb5a01be32b13707d35d1ddf9f920bd6983
casatoo <casato6666@gmail.com>
2022-08-03 12:08:50 +0900

    style:import ArrayList setting.xml

--- src/spring/di/program.java
+++ src/spring/di/program.java
@@ -1,9 +1,13 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class program {
@@ -27,6 +31,14 @@ public class program {
 		//ExamConsole console = (ExamConsole) context.getBean("console");
 		ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		// List<Exam> exams = new ArrayList<>();
+		List<Exam> exams = (List<Exam>) context.getBean("exams");
+		//exams.add(new NewlecExam(1,1,1,1));
+		
+		for(Exam e:exams) {
+			System.out.println(e);
+		}
 	}
 
 }
