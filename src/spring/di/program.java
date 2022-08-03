c6f50274d05f33eb4f0ab078c444ce450736124d
casatoo <casato6666@gmail.com>
2022-08-03 11:01:37 +0900

    style: using namespace p:

--- src/spring/di/program.java
+++ src/spring/di/program.java
@@ -3,6 +3,7 @@ package spring.di;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
 import spring.di.ui.ExamConsole;
 
 public class program {
@@ -19,6 +20,9 @@ public class program {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		// 호출방법 중에 classpathxml 방식을 많이 씀
 		// 다른 방법으로는 경로지정방식, 웹주소로 호출하는 방식 등등 이 있음
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		
 		//ExamConsole console = (ExamConsole) context.getBean("console");
 		ExamConsole console = context.getBean(ExamConsole.class);
